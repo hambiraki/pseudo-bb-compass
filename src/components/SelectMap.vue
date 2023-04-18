9a59cf9ae95bb6ad1e63e16c312a7bdce32a1e91
hambiraki <hambiraki@outlook.com>
2023-04-19 00:42:09 +0900

    situation初期表示の指定をSelectMapから消す(App.vueだけに)

--- src/components/SelectMap.vue
+++ src/components/SelectMap.vue
@@ -3,14 +3,14 @@
     <div class="celltitle" id="head_map">マップ</div>
     <div>
       <p>
-        <select v-model="selectedLocation">
+        <select v-model="location">
           <option v-for="locationOption in locations">
             {{ locationOption }}
           </option>
         </select>
       </p>
       <p>
-        <select v-model="selectedSituation">
+        <select v-model="situation">
           <option v-for="situationOption in situations">
             {{ situationOption }}
           </option>
@@ -21,27 +21,31 @@
 </template>
 
 <script setup lang="ts">
-import { Minimap } from "@/minimaps";
 import type { Situation } from "@/minimaps/minimap-names";
 import { situation2location, type Location } from "@/minimaps/minimap-names";
 import { ObjectValues, getKeyByValue } from "@/utils";
-import { computed, ref, watch, type Ref } from "vue";
+import { computed, ref, watch } from "vue";
+
+interface Props {
+  situation: Situation;
+}
+const props = defineProps<Props>();
 
 const locations = ObjectValues(situation2location);
-const selectedLocation: Ref<Location> = ref("スカービ渓谷");
+const situation = ref<Situation>(props.situation);
+const location = ref<Location>(situation2location[situation.value]);
 const situations = computed((): Situation[] => {
-  return getKeyByValue(situation2location, selectedLocation.value);
+  return getKeyByValue(situation2location, location.value);
 });
-const selectedSituation: Ref<Situation> = ref("戦線突破");
 watch(situations, (): void => {
-  selectedSituation.value = situations.value[0];
+  situation.value = situations.value[0];
 });
 
 interface Emits {
-  (event: "update:modelValue", minimap: Minimap): void;
+  (event: "update:situation", newSituation: Situation): void;
 }
 const emit = defineEmits<Emits>();
-watch(selectedSituation, (): void => {
-  emit("update:modelValue", new Minimap(selectedSituation.value));
+watch(situation, (): void => {
+  emit("update:situation", situation.value);
 });
 </script>
